c4341136a11d96c2c06f4a02df4a4d176b9b55d4
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2023-10-24 00:05:42 +0300

    Try to do ScalarMultiplication.

--- src/ScalarMul.cu
+++ src/ScalarMul.cu
@@ -4,7 +4,7 @@
  * Calculates scalar multiplication for block
  */
 __global__
-void ScalarMulBlock(int numElements, float* vector1, float* vector2, float *result) {
-
+void ScalarMulBlock(int num_items, float* vector1, float* vector2, float *result) {
+    VectorMul(num_items, vector1, vector2, result);
 }
 
