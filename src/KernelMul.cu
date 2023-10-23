4ee2295b1caf0e7876fabde23830fe7ba9280220
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2023-10-23 19:21:43 +0300

    Implement KernelMul.

--- src/KernelMul.cu
+++ src/KernelMul.cu
@@ -1,6 +1,16 @@
 #include <KernelMul.cuh>
 
 __global__ void KernelMul(int numElements, float* x, float* y, float* result) {
-  // your code here
+    // Аналогично KernelAdd.
+
+    uint3 thread_index_3d = threadIdx;
+    uint3 block_dim_3d = blockDim;
+
+    int thread_index = thread_index_3d.x;
+    int num_threads  = block_dim_3d.x;
+
+    for (int item_index = thread_index; item_index < num_items; item_index += num_threads) {
+        result[item_index] = x[item_index] * y[item_index];
+    }
 }
 
