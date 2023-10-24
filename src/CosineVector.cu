e96a884d4de2196821652c86bb69968f03a37070
Valeriy Zainullin <valeriy.zainullin@yandex.ru>
2023-10-24 05:34:50 +0300

    Fix compilation error.

--- src/CosineVector.cu
+++ src/CosineVector.cu
@@ -1,18 +1,18 @@
 #include <CosineVector.cuh>
 
-#include <ScalarMullRunner.cuh>
+#include <ScalarMulRunner.cuh>
 
 #include <cmath>
 
 float CosineVector(int num_items, float* vector1, float* vector2, int block_size) {
     // (v1, v2) = |v_1| |v_2| cos(v_1 ^ v_2)
-    float scalar_prod = ScalarMulSumPlusReduction(vector1, vector2, block_size);
+    float scalar_prod = ScalarMulSumPlusReduction(num_items, vector1, vector2, block_size);
 
-    float vector1_len2 = ScalarMulSumPlusReduction(vector1, vector1, block_size);
-    float vector1_len = std::sqrtf(vector1_len2);
+    float vector1_len2 = ScalarMulSumPlusReduction(num_items, vector1, vector1, block_size);
+    float vector1_len = std::sqrt(vector1_len2);
 
-    float vector2_len2 = ScalarMulSumPlusReduction(vector2, vector2, block_size);
-    float vector2_len = std::sqrtf(vector2_len2);
+    float vector2_len2 = ScalarMulSumPlusReduction(num_items, vector2, vector2, block_size);
+    float vector2_len = std::sqrt(vector2_len2);
 
     float angle = scalar_prod / (vector1_len * vector2_len);
 
